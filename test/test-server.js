e0ec4024b8379065d7ad0532323f07cacea324cf
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-08-24 10:50:48 -0500

    added tests for recipes

--- test/test-server.js
+++ test/test-server.js
@@ -29,7 +29,7 @@ describe('Shopping List', function() {
   // close our server at the end of these tests. Otherwise,
   // if we add another test module that also has a `before` block
   // that starts our server, it will cause an error because the
-  // server would still be running from the previous tests.
+ // server would still be running from the previous tests.
   after(function() {
     return closeServer();
   });
@@ -141,4 +141,90 @@ describe('Shopping List', function() {
         res.should.have.status(204);
       });
   });
-});
+}); 
+
+
+
+
+
+
+describe('Recipes', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+
+  it('should list items on GET', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+
+        res.body.length.should.be.at.least(1);
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+ 
+  it('should add an item on POST', function() {
+    const newItem = {name: 'coffee', checked: false};
+    return chai.request(app)
+      .post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.include.keys('id', 'name', 'ingredients');
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+
+  it('should update items on PUT', function() {
+
+    const updateData = {
+      name: 'foo',
+      ingredients: {"milk", "chocolate"}
+    };
+
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        updateData.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/shopping-list/${updateData.id}`)
+          .send(updateData);
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.deep.equal(updateData);
+      });
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
+}); 
